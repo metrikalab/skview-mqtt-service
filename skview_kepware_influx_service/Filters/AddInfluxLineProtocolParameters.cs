e59dac9ee888d30382bac5ad6a7612a0e4544091
DenisseD13 <kdenisse.durangmail.com>
2023-08-10 16:47:48 -0600

    feat(Influx): :sparkles: Add the settings for influx

---
+++ skview_kepware_influx_service/Filters/AddInfluxLineProtocolParameters.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace skview_kepware_influx_service.Filters
+{
+  public class AddInfluxLineProtocolParameters : IOperationFilter
+  {
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+      if (context.MethodInfo.Name == "WriteData")
+      {
+        operation.Parameters.Clear();
+
+        operation.Parameters.Add(new OpenApiParameter
+        {
+          Name = "measurement",
+          In = ParameterLocation.Query,
+          Required = true,
+          Schema = new OpenApiSchema
+          {
+            Type = "string"
+          }
+        });
+
+        operation.Parameters.Add(new OpenApiParameter
+        {
+          Name = "densidad",
+          In = ParameterLocation.Query,
+          Required = true,
+          Schema = new OpenApiSchema
+          {
+            Type = "number",
+            Format = "double"
+          }
+        });
+
+        operation.Parameters.Add(new OpenApiParameter
+        {
+          Name = "flujo",
+          In = ParameterLocation.Query,
+          Required = true,
+          Schema = new OpenApiSchema
+          {
+            Type = "number",
+            Format = "double"
+          }
+        });
+
+        operation.Parameters.Add(new OpenApiParameter
+        {
+          Name = "presion",
+          In = ParameterLocation.Query,
+          Required = true,
+          Schema = new OpenApiSchema
+          {
+            Type = "number",
+            Format = "double"
+          }
+        });
+
+        operation.Parameters.Add(new OpenApiParameter
+        {
+          Name = "temperaturaInstantanea",
+          In = ParameterLocation.Query,
+          Required = true,
+          Schema = new OpenApiSchema
+          {
+            Type = "number",
+            Format = "double"
+          }
+        });
+      }
+    }
+  }
+}
